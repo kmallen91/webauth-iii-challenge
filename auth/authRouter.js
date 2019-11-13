79d6f6d1daffc342a51b6ab16d459d7c189e544e
Kevin Allen <kevin.allen91@gmail.com>
2019-11-13 13:34:21 -0800

    added middleware to routes, mvp complete

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -3,11 +3,45 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const users = require("../users/users-model.js");
+const validateUser = require("../middleware/validateUser");
+const getJwtToken = require("../middleware/getJwtToken");
 
 router.post("/register", (req, res) => {
   let user = req.body;
+  const validateResult = validateUser(user);
+
+  if (validateResult.isSuccessful === true) {
+    let hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+
+    users
+      .add(user)
+      .then(saved => res.status(201).json(saved))
+      .catch(err =>
+        res.status(500).json({ message: `error registering user` })
+      );
+  } else {
+    res
+      .status(400)
+      .json({ message: `invalid information`, errors: validateResult(errors) });
+  }
 });
 
 router.post("/login", (req, res) => {
   let { username, password } = req.body;
+
+  users
+    .findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = getJwtToken(user.username);
+        res.status(200).json({ message: `Welcome ${user.username}`, token });
+      } else {
+        res.status(401).json({ message: `invalid credentials` });
+      }
+    })
+    .catch(err => res.status(500).json({ message: `error logging in` }));
 });
+
+module.exports = server;
